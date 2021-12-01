ce2c1b5d73a872d5efb53c970776320c48acf3e2
Paul <paul.constantin.chitic@gmail.com>
2021-12-01 06:43:28 +0200

    CompanyProfile refactor

--- src/components/company-profile/index.tsx
+++ src/components/company-profile/index.tsx
@@ -38,10 +38,10 @@ const CompanyProfile = ({ symbol, getCompanyProfile }: Props) => {
             {!loading && companyProfile && <>
                 <h3>{COMPANY_PROFILE}</h3>
                 <div className="company-profile-data">
-                    <div className="company-profile-item">{companyProfile.description || ''}</div>
-                    <div className="company-profile-item">{companyProfile.country || ''}</div>
-                    <div className="company-profile-item">{companyProfile.state || ''}</div>
-                    <div className="company-profile-item">{companyProfile.city || ''}</div>
+                    <div className="company-profile-item">{companyProfile.description}</div>
+                    <div className="company-profile-item">{companyProfile.country}</div>
+                    <div className="company-profile-item">{companyProfile.state}</div>
+                    <div className="company-profile-item">{companyProfile.city}</div>
                 </div>
             </>}
         </article>
